c4e2dc0c83ade9f75b4de18347a043a507a0baa1
RaulPC01 <129307992+RaulPC01@users.noreply.github.com>
2023-03-30 16:46:45 +0200

    Add files via upload

---
+++ Enfermedad.java
@@ -0,0 +1,35 @@
+public class Enfermedad {
+    private String nombre;
+    private String[] sintomas;
+    private int gravedad;
+    private boolean curada;
+
+    public Enfermedad(String nombre, String[] sintomas, int gravedad) {
+        this.nombre = nombre;
+        this.sintomas = sintomas;
+        this.gravedad = gravedad;
+        this.curada = false;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String[] getSintomas() {
+        return this.sintomas;
+    }
+
+    public int getGravedad() {
+        return this.gravedad;
+    }
+
+    public boolean siEstaCurada() {
+        return this.curada;
+    }
+
+    public void curar() {
+        this.curada = true;
+ }
+    
+    
+}
